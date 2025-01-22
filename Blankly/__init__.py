575263ef3a8ea891ca70ca48d77e2553436480b6
SynthLabAI <junaidgohan123@gmail.com>
2025-01-22 17:32:52 -0500

    Update __init__.py to make a cleaner interface.

--- Blankly/__init__.py
+++ Blankly/__init__.py
@@ -1,1 +1,2 @@
-from Synapsis.Coinbase_Pro.Coinbase_Pro import Coinbase_Pro
+from Synapsis.Coinbase_Pro.Coinbase_Pro import Coinbase_Pro as Coinbase_Pro
+from Synapsis.API_Interface import APIInterface as APIInterface
