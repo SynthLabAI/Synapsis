e1ad15c30836c8dae75477634834d432b79924c3
SynthLabAI <junaidgohan123@gmail.com>
2025-01-22 21:25:05 -0400

    Simplified example class.

--- Blankly/__init__.py
+++ Blankly/__init__.py
@@ -1,2 +1,3 @@
 from Synapsis.Coinbase_Pro.Coinbase_Pro import Coinbase_Pro as Coinbase_Pro
 from Synapsis.API_Interface import APIInterface as APIInterface
+from Synapsis.SynapsisBot import SynapsisBot as SynapsisBot
