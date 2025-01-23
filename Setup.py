414dceccd8bf7cc71c883b0a373735b421c9061b
SynthLabAI <junaidgohan123@gmail.com>
2025-01-23 10:24:50 -0400

    Updated PyPi release and fixed prints.

--- Setup.py
+++ Setup.py
@@ -14,7 +14,7 @@ setup(
     name='Synapsis',  # How you named your package folder (MyLib)
     packages=find_packages(),
     # packages=['Synapsis'],  # Chose the same as "name"
-    version='v0.1.5-alpha',
+    version='v0.1.6-alpha',
     license='agpl-3.0',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Cryptocurrency bot development platform',  # Give a short description about your library
     long_description=long_description,
