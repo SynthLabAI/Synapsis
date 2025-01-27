0c66f2889853564a2004791cecdded1fbafc1ac1
SynthLabAI <junaidgohan123@gmail.com>
2025-01-27 02:59:13 -0400

    Brandon & Emerson drafting for interface

--- Blankly/strategy/strategy_base.py
+++ Blankly/strategy/strategy_base.py
@@ -1,9 +1,10 @@
 import Synapsis
+from Synapsis.utils.time_builder import time_interval_to_seconds
+
 
 class Strategy(Synapsis.SynapsisBot):
     def main(self, args):
-        self.ticker = self.Ticker_manager.get_ticker()
-        self.orderbook = self.Orderbook_manager.get_ticker()
+        pass
 
     def add_price_event(self, callback, resolution):
         """
@@ -12,12 +13,27 @@ class Strategy(Synapsis.SynapsisBot):
             callback: The price event callback that will be added to the current ticker and run at the proper resolution
             resolution: The resolution that the callback will be run
         """
-        self.ticker.append_callback(Synapsis.Scheduler(callback, resolution))
+        if time_interval_to_seconds(resolution) < 60:
+            self.Ticker_Manager.append_callback(callback)
+            # Synapsis.Scheduler(self.price_event_websocket, resolution, callback=callback)
+        else:
+            Synapsis.Scheduler(self.price_event_rest, resolution, callback=callback)
 
-    def add_orderbook_event(self, callback):
+    def add_orderbook_event(self, callback, resolution):
         """
         Add Orderbook Event
         Args:
-            callback: The Orderbook callback that will be added to the current ticker orderbook
+            callback: The price event callback that will be added to the current ticker and run at the proper resolution
+            resolution: The resolution that the callback will be run
         """
-        self.orderbook.append_callback(callback)
+        self.Orderbook_Manager.append_callback(callback)
+
+    def price_event_rest(self, callback):
+        price = self.Interface.get_price(self.currency_pair)
+        callback(price, self.currency_pair)
+        return
+
+    def price_event_websocket(self, callback):
+        price = self.Ticker_Manager.get_most_recent_tick()
+        callback(price, self.currency_pair)
+        return
