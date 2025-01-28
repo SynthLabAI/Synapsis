04c7d9924ffc1ea3a1cd8ffdbb14740b0fdab81c
SynthLabAI <junaidgohan123@gmail.com>
2025-01-27 21:19:02 -0400

    Initial backtesting done.

--- Blankly/indicators/__init__.py
+++ Blankly/indicators/__init__.py
@@ -1,2 +1,5 @@
 from Synapsis.indicators.indicators import *
 from Synapsis.indicators.utils import *
+from Synapsis.indicators.moving_averages import *
+from Synapsis.indicators.oscillators import *
+from Synapsis.indicators.statistics import *
