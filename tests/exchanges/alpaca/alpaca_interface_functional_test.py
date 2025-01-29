4053c6c377b14b02db8d2036af8de756bae05287
SynthLabAI <junaidgohan123@gmail.com>
2025-01-29 06:56:45 -0700

    started alpaca functional tests

---
+++ tests/exchanges/alpaca/alpaca_interface_functional_test.py
@@ -0,0 +1,20 @@
+from Synapsis.auth.Alpaca.auth import AlpacaAuth
+from Synapsis.auth.direct_calls_factory import InterfaceFactory
+import pytest
+
+from Synapsis.exchanges.Alpaca.alpaca_api_interface import AlpacaInterface
+from tests.helpers.comparisons import is_sub_dict
+from pathlib import Path
+
+@pytest.fixture
+def alpaca_interface() -> None:
+    keys_file_path = Path("tests/config/keys.json").resolve()
+    settings_file_path = Path("tests/config/settings.json").resolve()
+
+    auth_obj = AlpacaAuth(keys_file_path, "alpaca test portfolio")
+    _, alpaca_interface = InterfaceFactory.create("alpaca", auth_obj, settings_file_path)
+    return alpaca_interface
+
+
+def test_get_exchange(alpaca_interface: AlpacaInterface) -> None:
+    assert alpaca_interface.get_exchange_type() == 'alpaca'
