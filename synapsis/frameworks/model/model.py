27f384f9c6d6d7ffbbac0d7bee6e7807bbe76865
SynthLabAI <junaidgohan123@gmail.com>
2025-02-18 11:25:43 -0400

    Weaken tests and allow sleep to be a time interval

--- synapsis/frameworks/model/model.py
+++ synapsis/frameworks/model/model.py
@@ -22,6 +22,7 @@ from synapsis.exchanges.interfaces.paper_trade.backtest_controller import BackTe
 from synapsis.exchanges.interfaces.paper_trade.abc_backtest_controller import ABCBacktestController
 from synapsis.exchanges.interfaces.paper_trade.paper_trade import PaperTrade
 from synapsis.exchanges.exchange import Exchange
+from synapsis.utils.time_builder import time_interval_to_seconds
 import time
 
 
@@ -82,7 +83,8 @@ class Model:
         else:
             return self.__backtester.time
 
-    def sleep(self, seconds: [int, float]):
+    def sleep(self, seconds: [int, float, str]):
+        seconds = time_interval_to_seconds(seconds)
         if not self.is_backtesting:
             time.sleep(seconds)
         else:
