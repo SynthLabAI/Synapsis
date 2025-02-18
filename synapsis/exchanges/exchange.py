46d0115ecd9d73d13cdf0542099669029b9e14f5
SynthLabAI <junaidgohan123@gmail.com>
2025-02-18 11:44:43 -0400

    okx interface + websockets fully done

--- synapsis/exchanges/exchange.py
+++ synapsis/exchanges/exchange.py
@@ -28,7 +28,7 @@ from synapsis.exchanges.interfaces.ftx.ftx_interface import FTXInterface
 from synapsis.exchanges.interfaces.alpaca.alpaca_interface import AlpacaInterface
 from synapsis.exchanges.interfaces.binance.binance_interface import BinanceInterface
 from synapsis.exchanges.interfaces.kucoin.kucoin_interface import KucoinInterface
-from synapsis.exchanges.interfaces.okx.okx_interface import OkexInterface
+from synapsis.exchanges.interfaces.okx.okx_interface import OkxInterface
 
 
 
@@ -82,7 +82,7 @@ class Exchange(ABCExchange, abc.ABC):
         elif self.__type == "kucoin":
             self.interface = KucoinInterface(self.__type, calls)
         elif self.__type == "okx":
-            self.interface = OkexInterface(self.__type, calls)
+            self.interface = OkxInterface(self.__type, calls)
 
         synapsis.reporter.export_used_exchange(self.__type)
 
