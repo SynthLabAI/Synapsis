d34432e76935d5ef98143ba6fe2a73660c4b33fa
SynthLabAI <junaidgohan123@gmail.com>
2025-02-21 22:55:25 -0400

    Add futures imports

--- synapsis/__init__.py
+++ synapsis/__init__.py
@@ -44,6 +44,10 @@ import synapsis.indicators as indicators
 from synapsis.utils import time_builder
 
 from synapsis.enums import Side, OrderType, OrderStatus, TimeInForce
+from synapsis.exchanges.interfaces.binance_futures.binance_futures import BinanceFutures
+from synapsis.exchanges.interfaces.ftx_futures.ftx_futures import FTXFutures
+from synapsis.frameworks.strategy import FuturesStrategy
+from synapsis.frameworks.strategy import FuturesStrategyState
 
 from synapsis.deployment.reporter_headers import Reporter as __Reporter_Headers
 
